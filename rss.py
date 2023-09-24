70c0a8aa48cccbff30bfc5d270234d7072b42c36
Valéry Marchive <33423235+Casualtek@users.noreply.github.com>
2023-09-24 22:19:10 +0200

    Update rss.py

--- rss.py
+++ rss.py
@@ -154,7 +154,8 @@ def main():
             continue
         mark_item_seen(item_hash)
 
-        title = translate_text(entry.title)
+#        title = translate_text(entry.title)
+        title = entry.title
         link  = decode_google_news_url(entry.link)
         date  = entry.published
 
