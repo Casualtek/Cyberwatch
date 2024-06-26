d6ff58c40593f2dc6f8e7004acbe42d4e70fa071
Valéry Marchive <33423235+Casualtek@users.noreply.github.com>
2024-06-26 12:35:10 +0200

    Update notify.py

--- notify.py
+++ notify.py
@@ -96,13 +96,13 @@ def main():
     country    = story['country']
 
     tweet = flag.flag(countryISO[country])+' '+story['victim']+' ('+story['domain']+')'+' a été victime d\'une #cyberattaque autour du '+date_tweet+'.\n⏭️ https://t.ly/t23z2\n👉 '+story['url']+' cc @ransomwaremap @cyber_etc'
-#    post_to_twitter(tweet)
+    post_to_twitter(tweet)
 
     telegram_msg = flag.flag(countryISO[country])+' '+story['victim']+' ('+story['domain']+')'+' a été victime d\'une cyberattaque autour du '+date_tweet+'.\n\n'+story['summary']+'\n\n👉 [source]('+story['url']+')'
-#    post_to_telegram(telegram_msg)
+    post_to_telegram(telegram_msg)
 
     mastodon_msg = flag.flag(countryISO[country])+' '+story['victim']+' ('+story['domain']+')'+' a été victime d\'une cyberattaque autour du '+date_tweet+'.\n\n'+story['summary']+'\n\n👉 '+story['url']
-#    post_to_mastodon(mastodon_msg)
+    post_to_mastodon(mastodon_msg)
 
     post = flag.flag(countryISO[country])+' '+story['victim']+' ('+story['domain']+')'+' a été victime d\'une cyberattaque autour du '+date_tweet+'.\n👉 '
     post_to_bluesky(post,story['url'])
