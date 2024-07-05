574a8493d796586edcec74e4517f855446b3986e
Valéry Marchive <33423235+Casualtek@users.noreply.github.com>
2024-07-05 12:25:53 +0200

    Update review-week-de.py

--- review-week-de.py
+++ review-week-de.py
@@ -30,7 +30,7 @@ def ask_chatgpt(news_count, most_hit_country, countries):
 
     print(f'Obtaining introduction.')
     completion = openai.chat.completions.create(
-        model='gpt-4-1106-preview',
+        model='gpt-4o',
         messages=messages,
         max_tokens=180,
         n=1,
@@ -44,7 +44,7 @@ def ask_chatgpt(news_count, most_hit_country, countries):
 
     print(f'Obtaining summary.')
     completion = openai.chat.completions.create(
-        model='gpt-4-1106-preview',
+        model='gpt-4o',
         messages=messages,
         max_tokens=30,
         n=1,
@@ -58,7 +58,7 @@ def ask_chatgpt(news_count, most_hit_country, countries):
 
     print(f'Obtaining title.')
     completion = openai.chat.completions.create(
-        model="gpt-4-1106-preview",
+        model="gpt-4o",
         messages=messages,
         max_tokens=90,
         n=1,
